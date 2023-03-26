a424cf15ca3f4a0ec492b193847e95ca7054d9b9
schons <schonsluuiz@gmail.com>
2023-03-26 18:13:41 -0300

    searching IP with the address of the host

--- app/app.go
+++ app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Create creates a new cli.App
 func Generate() *cli.App {
@@ -8,5 +14,35 @@ func Generate() *cli.App {
 	app.Name = "CLI Search IP"
 	app.Usage = "Search IP and Server Names"
 	app.Version = "0.0.1"
+	
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "ip",
+			Usage:  "Search IP",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "go.dev",
+				},
+			},
+			Action: searchIP,
+		},
+	}
+
 	return app
+}
+
+
+
+func searchIP(c *cli.Context) {
+	host := c.String("host")
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+
 }
